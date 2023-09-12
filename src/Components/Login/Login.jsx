d5aba3ff54b4452f1bcb4f91e47c69ac0551857e
Ankit <ankitchauhan21500@gmail.com>
2023-09-12 16:00:40 +0530

    added forgot passwords functionality

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  , {useState} from 'react';
 import "./Login.css"
 import { initializeApp } from "firebase/app";
 import firebaseConfig from '../../firebaseConfig'
@@ -7,11 +7,13 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 
 import { Helmet } from 'react-helmet';
 
 export default function Login() {
+
+  const [passEmail, setpassEmail] = useState("")
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
@@ -32,6 +34,23 @@ export default function Login() {
     }
   }
 
+  const forgotPass = async () => {
+    console.log("this is happening");
+    await sendPasswordResetEmail(auth, passEmail);
+    try{
+      toast.success("Email Sent please check you mail account" , {autoClose : 1500});
+    }
+    catch{
+      toast.error("Something went wrong" , {autoClose : 1500});
+    }
+
+  }
+
+  const handleEmail = (e)=>{
+    console.log(e.target.value);
+    setpassEmail(e.target.value);
+  }
+
   return (
     <main className="Login_main">
 
@@ -45,12 +64,14 @@ export default function Login() {
       </h1>
       <h2>~ Login to you CodeScribe NIT-B account ~</h2>
       <form onSubmit={handleLogin} action="">
-        <input type="email" placeholder='Email' name="email" />
+        <input onChange={handleEmail} type="email" placeholder='Email' name="email" />
         <input placeholder='Password' type="password" name="password" />
         <button>Login</button>
       </form>
       <h2>Don't have an account? Sign Up here</h2>
-      <button onClick={()=> { window.location.href = `/signup`}} >Sign Up</button>
+      <button onClick={() => { window.location.href = `/signup` }} >Sign Up</button>
+      <br />
+      <button onClick={ ()=> {forgotPass()}} >Forgot Password?</button>
       <br />
       <br />
     </main>
